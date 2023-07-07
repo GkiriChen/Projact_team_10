7b0b9a3c7efb57f1c3a210bac77f9b5e8e95f2f1
Serhii Nesterenko <sy.nesterenko@gmail.com>
2023-07-07 18:40:14 +0300

    Update start_page.py

--- Test/start_page.py
+++ Test/start_page.py
@@ -7,14 +7,13 @@ def input_text():
     text = colored('Зробіть свій вибір: ', 'yellow')
     return input(text).lower().split(' ')
 
-def show_greeting():    
-   
+def show_greeting():      
      
     x = PrettyTable(align='l')    # ініціалізуєм табличку, вирівнюєм по лівому краю 
 
     x.field_names = [colored("Вас вітає бот помічник, наразі доступні наступні модулі:", 'light_blue')]
     x.add_row([colored("1. Сортування файлів","blue")])     
-    x.add_row([colored("2. Роботаз адресною книгою","blue")])
+    x.add_row([colored("2. Робота з адресною книгою","blue")])
     x.add_row([colored("3. Робота з нотатками","blue")])
     x.add_row([colored("0. Закінчити роботу програми","blue")])
 
@@ -37,24 +36,98 @@ def run():
                 if int(answer[0]) == 0:
                     cprint("Good bye!", 'blue')
                     break
-                if answer[0] == 1:
+                if int(answer[0]) == 1:
                     sorting = True
-                if answer[0] == 2:
+                if int(answer[0]) == 2:
                     addresbook = True
-                if answer[0] == 3:
+                if int(answer[0]) == 3:
                     notes = True
             except ValueError as e:
                 cprint('Введіть будь ласка число від 0 до 3', 'red')
 
-        while sorting:
+        if sorting:
+            #  тут буде виклик логіки сортування
             sorting = False
             os.system('cls||clear')  # чистим консоль
         
-        while addresbook:
+        if addresbook:
+            #  тут буде виклик логіки роботи з контактами
             addresbook = False
         
-        while notes:
+        if notes:
+            #  тут буде виклик логіки роботи з нотатками            
             notes = False
 
 if __name__ == '__main__':
     run()
+
+# from termcolor import colored, cprint
+# from prettytable import PrettyTable
+# import os
+
+# class MenuOption:
+#     def __init__(self, description):
+#         self.description = description
+
+#     def execute(self):
+#         raise NotImplementedError("execute() method must be implemented in subclasses.")
+
+
+# class SortingOption(MenuOption):
+#     def execute(self):
+#         print("Sorting module selected.")
+#         # Add sorting module logic here
+
+
+# class AddressBookOption(MenuOption):
+#     def execute(self):
+#         print("Address book module selected.")
+#         # Add address book module logic here
+
+
+# class NotesOption(MenuOption):
+#     def execute(self):
+#         print("Notes module selected.")
+#         # Add notes module logic here
+
+
+# class Menu:
+#     def __init__(self):
+#         self.options = [
+#             MenuOption("Exit program"),
+#             SortingOption("Sorting files"),
+#             AddressBookOption("Address book"),
+#             NotesOption("Notes")
+#         ]
+
+#     def show(self):
+#         table = PrettyTable(field_names=[colored("Available Modules", 'light_blue')])
+#         for option_index, option in enumerate(self.options):
+#             table.add_row([f"{option_index}. {option.description}"])
+#         print(table)
+
+#     def run(self):
+#         os.system('cls||clear')
+#         while True:
+#             self.show()
+#             choice = self.get_choice()
+#             if choice == 0:
+#                 cprint("Goodbye!", 'blue')
+#                 break
+#             elif 0 < choice < len(self.options):
+#                 self.options[choice].execute()
+#             else:
+#                 cprint("Invalid choice. Please enter a valid option.", 'red')
+
+#     def get_choice(self):
+#         while True:
+#             try:
+#                 choice = int(input("Enter your choice: "))
+#                 return choice
+#             except ValueError:
+#                 cprint("Invalid input. Please enter a number.", 'red')
+
+
+# if __name__ == '__main__':
+#     menu = Menu()
+#     menu.run()
