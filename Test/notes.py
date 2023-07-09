72eb171bdce7e13f0812c75e5aa00fe95c827b02
Ghendeler <jian.bakhman@gmail.com>
2023-07-09 22:03:53 +0300

    Added notes.py

--- Test/notes.py
+++ Test/notes.py
@@ -1,19 +1,23 @@
 from collections import UserDict
 from datetime import datetime
 import pickle
-
+import time
 
 class Notes(UserDict):
     filename = 'notes.sav'
-    notes = {}
+    MAX_STR_LEN = 50
+    # notes = {}
 
     def add_note(self, note):
         note = Note(note)
-        print(note.datetime)
         self.data[note.datetime] = note
 
-    def find_in_notes(self):
-        pass
+    def find_in_notes(self, string):
+        res = {}
+        for k, v in self.data.items():
+            if v.text.lower().find(string.lower()) >= 0:
+                res[k] = v
+        return res
 
     def edit_note(self):
         pass
@@ -24,12 +28,17 @@ class Notes(UserDict):
     def find_by_tag(self):
         pass
 
-    def show_notes(self):
-        res = '-' * 20 + '\n'
-        for k, v in self.data.items():
-            res += v.text + '\n'
-            res += k.strftime("%d-%m-%Y") + '\n'
-            res += '-' * 20 + '\n'
+    def show_notes(self, data = None):
+        if not data:
+            data = self.data
+        res = '-' * 30 + '\n'
+        for k, v in data.items():
+            t = v.text
+            while len(t):
+                res += t[:self.MAX_STR_LEN] + '\n'
+                t = t[50:]
+            res += k.strftime("<%d-%m-%Y %H:%M>") + '\n'
+            res += '-' * 30 + '\n'
         return res
 
     def iterator(self, step=5):
@@ -48,20 +57,9 @@ class Notes(UserDict):
             content = pickle.load(file)
         return content
 
-    # def find(self, string):
-    #     res = []
-    #     for k, v in self.data.items():
-    #         if k.find(string) >= 0:
-    #             res.append((k, v)) 
-    #         else:
-    #             for phone in v.phones:
-    #                 if phone.find(string):
-    #                     res.append((k, v))
-    #     return res
-
 
 class Note:
-    MAX_NOTE_LEN = 50   # max lenth of note
+    MAX_NOTE_LEN = 150   # max lenth of note
 
     def __init__(self, note):
         self.text = note[:self.MAX_NOTE_LEN]
