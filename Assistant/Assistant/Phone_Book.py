cd9673d33796bd989aa40ff3648546d0ac65979e
GkiriChen <128749954+GkiriChen@users.noreply.github.com>
2023-07-12 22:08:10 +0300

    update

--- Assistant/Assistant/Phone_Book.py
+++ Assistant/Assistant/Phone_Book.py
@@ -76,8 +76,12 @@ class AddressBook(UserDict):
         
     def add_contact(self, args):
         name = args[0]
-        record = Record(Name(name))
         
+        contact_in = self.data.get(name)
+        if contact_in:
+            return "Такий контакт вже існує"
+        record = Record(Name(name))
+           
         for item in args[1:]:
             if item.startswith("bd="):
                 birthday_value = item.split("=")[1]
@@ -334,7 +338,7 @@ class Record:
 
 file_name = 'Address_Book.bin'
 # commands = ['add', 'change', 'phones', 'hello', 'show_all', 'next', 'del_phone', 'del_contact', 'change_email', 'change_bd', 'edit_contact', 'search', 'help', 'change_address', 'birthday_in_days', 'exit']
-commands = ['add', 'phones', 'show_all', 'next', 'del_contact', 'edit_contact', 'search', 'birthday_in_days', 'help', 'exit']
+commands = ['add', 'phones', 'show_all', 'next', 'del_phone', 'del_contact', 'edit_contact', 'search', 'birthday_in_days', 'help', 'exit']
 
 def show_help():      
     x = PrettyTable(align='l')    # ініціалізуєм табличку, вирівнюєм по лівому краю 
@@ -378,7 +382,7 @@ def input_error(func):
 @input_error
 def add_contact(args):   
     global phone_book
-    return phone_book.add_contact(args) 
+    cprint(phone_book.add_contact(args), 'blue')
     
 
 @input_error     
@@ -438,12 +442,12 @@ def edit_contact(args):
 
 @input_error
 def show():
-    return next(phone_book.iterator())
+   cprint(next(phone_book.iterator()), 'green')
 
 @input_error
 def birthday_in_days(args):
     global phone_book
-    phone_book.birthday_in_days(args)
+    return phone_book.birthday_in_days(args)
 
 @input_error
 def main():
@@ -476,13 +480,13 @@ def main():
         elif b == 'help' or d == 'help':
             print(show_help())
         elif b == 'next' or d == 'next':
-            print(show())
+            show()
         elif d == 'birthday_in_days':
             birthday_in_days(args)
         elif b in commands:
             cprint('Enter arguments to command', 'red')
         elif d == 'add':
-            cprint(add_contact(args), 'blue')
+            add_contact(args)
         # elif d == 'change':
         #     cprint(change_contact(args), 'green')
         # elif d == 'change_email':
@@ -491,8 +495,8 @@ def main():
             # cprint(change_birthday(args), 'green')
         elif d == 'phones':
             print(phone_book.show_phones(args))
-        # elif d == 'del_phone':
-        #     print(del_phone(args))
+        elif d == 'del_phone':
+            print(del_phone(args))
         elif d == 'search':
             print(search(args))
         elif d == 'del_contact':
